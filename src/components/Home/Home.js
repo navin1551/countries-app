55ae8f5d9fd733ea23ae648e8347e74db966d187
Navin Kumar <navin1551@gmail.com>
2019-10-12 17:16:27 -0700

    working on slideshow

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SlideShow from "../SlideShow/SlideShow";
+import slide1 from "../../assets/slide1.jpg";
+import slide2 from "../../assets/slide2.jpg";
+import slide3 from "../../assets/slide3.jpg";
+import slide4 from "../../assets/slide4.jpg";
+import slide5 from "../../assets/slide5.jpg";
 import "./Home.css";
+
+const s = {
+  container: "screenW screenH dGray col",
+  header: "flex1 fCenter fSize2",
+  main: "flex8 white",
+  footer: "flex1 fCenter"
+};
+
+const slides = [slide1, slide2, slide3, slide4, slide5];
 
 export default class Home extends React.Component {
   render() {
@@ -10,10 +25,10 @@ export default class Home extends React.Component {
           <p className="home-title">Welcome to Countries!</p>
         </div>
         <div>
-          <p className="home-info">
-            View different countries and information and put together a travel
-            itinerary
-          </p>
+          <p className="home-info">View different countries and information</p>
+        </div>
+        <div className={s.main}>
+          <SlideShow slides={slides} />
         </div>
         <div id="home-link">
           <Link to="/search">Lets Start</Link>
